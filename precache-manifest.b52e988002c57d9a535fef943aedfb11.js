50c522dbbd6209ee4ae1657310cc06e3ba084db3
Andre Lima <hello@andrelima.space>
2018-11-11 23:44:06 -0200

    Updates

---
+++ precache-manifest.b52e988002c57d9a535fef943aedfb11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f449844ec9f2af83ff",
+    "url": "/musicmaster/static/js/runtime~main.76f44984.js"
+  },
+  {
+    "revision": "063aa6d2ed0b7ef3265f",
+    "url": "/musicmaster/static/js/main.063aa6d2.chunk.js"
+  },
+  {
+    "revision": "731d5d6f3d0ebb48fcd7",
+    "url": "/musicmaster/static/js/1.731d5d6f.chunk.js"
+  },
+  {
+    "revision": "063aa6d2ed0b7ef3265f",
+    "url": "/musicmaster/static/css/main.2384dd6b.chunk.css"
+  },
+  {
+    "revision": "ee70fb6f1a7724a682e3475d9fd9b201",
+    "url": "/musicmaster/index.html"
+  }
+];
